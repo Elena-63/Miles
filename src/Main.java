4c821f676a5b232107dffde2f3855f9a73c31906
Domanova Elena <domanova_eg@mail.ru>
2021-04-12 17:56:25 +0400

    update_files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int costTikets = 4_440;
+        int bonusMiles = 20;
+        int totalMiles = costTikets / bonusMiles;
+        System.out.println(totalMiles);
+    }
+}
